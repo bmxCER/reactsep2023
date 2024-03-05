2598b061c9067dd715f5264778040a4a683c5399
retro <retrowew@gmail.com>
2024-03-05 13:00:33 +0100

    hw 7

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,46 +1,42 @@
+// Header.js
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { carsActions } from "../store";
-
 import { carsService } from "../services";
 
 const Header = () => {
     const dispatch = useDispatch();
-    const {cars} = useSelector(state => state.cars);
+    const { cars } = useSelector(state => state.cars);
     const [brand, setBrand] = useState("");
     const [price, setPrice] = useState("");
     const [year, setYear] = useState("");
 
     const handleSubmit = async (event) => {
-        event.preventDefault();
+        event.preventDefault()
 
-        setBrand("");
-        setPrice("");
-        setYear("");
-
-    };
-
-    const save = () => {
         const newCar = {
             brand,
             price: parseInt(price),
             year: parseInt(year)
-        };
+        }
 
-        const response = carsService.create(newCar);
-        dispatch(carsActions.setResponse([...cars ,response.data]));
 
-        dispatch(carsActions.updateCar([response.data]));
+        const response = await carsService.create(newCar)
+
+        dispatch(carsActions.setResponse([...cars, response.data]))
+
+
+        dispatch(carsActions.updateCar([response.data]))
+
+
+        setBrand("")
+        setPrice("")
+        setYear("")
     }
-
-    const update = async (updatedCar) => {
-        await dispatch(carsActions.updateCar(updatedCar))
-    }
-
 
     return (
         <div>
-            <form onSubmit={handleSubmit? save : update}>
+            <form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="brand"
